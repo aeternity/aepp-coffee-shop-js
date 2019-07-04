ff74953a8e519902a976a157f0c574f288abb8b0
Aleksey <alexeitcekov@abv.bg>
2019-07-04 10:54:56 +0300

    updated main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -19713,56 +19713,15 @@ const Impl = __webpack_require__(463);
 /* 183 */
 /***/ (function(module, exports) {
 
-// 3001
-// const API_URL = 'http://localhost:3001';
-// const INTERNAL_API_URL = 'http://localhost:3113';
-// const STATE_CHANNEL_URL = 'ws://localhost:3001/channel'
-// const NETWORK_ID = 'ae_devnet';
-// const RESPONDER_HOST = 'localhost';
-// const RESPONDER_PORT = 3001;
 
-// http://ae-coffee-shop-node-multi.eu-west-1.elasticbeanstalk.com
-// const API_URL = 'http://ae-coffee-shop-node-multi.eu-west-1.elasticbeanstalk.com';
-// const INTERNAL_API_URL = 'http://ae-coffee-shop-node-multi.eu-west-1.elasticbeanstalk.com:3113';
-// const STATE_CHANNEL_URL = 'ws://ae-coffee-shop-node-multi.eu-west-1.elasticbeanstalk.com/channel'
-// const NETWORK_ID = 'ae_uat';
-// const RESPONDER_HOST = 'localhost';
-// const RESPONDER_PORT = 3333;
-
-// HTTPS - 
-// https://limenode.daepps.tech
-// const API_URL = 'https://limenode.daepps.tech';
-// const INTERNAL_API_URL = 'https://limenode.daepps.tech:3113';
-// const STATE_CHANNEL_URL = 'wss://limenode.daepps.tech/channel'
-// const NETWORK_ID = 'ae_uat';
-// const RESPONDER_HOST = 'localhost';
-// const RESPONDER_PORT = 3333;
-
-// HTTP
-// http://limenode.daepps.tech
-// const API_URL = 'http://limenode.daepps.tech';
-// const INTERNAL_API_URL = 'http://limenode.daepps.tech:3113';
-// const STATE_CHANNEL_URL = 'ws://limenode.daepps.tech/channel'
-// const NETWORK_ID = 'ae_uat';
-// const RESPONDER_HOST = 'localhost';
-// const RESPONDER_PORT = 3333;
-
-// HTTPS - 
-// https://limeaenode.daepps.tech /v2/status 
+// HTTPS
+// https://limeaenode.daepps.tech/v2/status
 const API_URL = 'https://limeaenode.daepps.tech';
 const INTERNAL_API_URL = 'https://limeaenode.daepps.tech:3113';
 const STATE_CHANNEL_URL = 'wss://limeaenode.daepps.tech/channel'
 const NETWORK_ID = 'ae_uat';
 const RESPONDER_HOST = 'localhost';
 const RESPONDER_PORT = 3333;
-
-// // test net // https://sdk-testnet.aepps.com
-// const API_URL = 'https://sdk-testnet.aepps.com';
-// const INTERNAL_API_URL = 'https://sdk-testnet.aepps.com/internal';
-// const STATE_CHANNEL_URL = 'wss://sdk-testnet.aepps.com/channel'
-// const NETWORK_ID = 'ae_uat';
-// const RESPONDER_HOST = 'localhost';
-// const RESPONDER_PORT = 3333;
 
 module.exports = {
     API_URL,
@@ -19777,28 +19736,10 @@ module.exports = {
 /* 184 */
 /***/ (function(module, exports) {
 
-// module.exports = {
-//     publicKey: 'ak_27c4YpfUuW4s9T6RBNpASNuDRiXSWQ93uZ5WxaUXfx1Lqibv33',
-//     secretKey: '357985aa340a6dc64ad7fff331d478c9d2cee98f3cd5fbe1dd6b3c239647271392b142f128defdf72b6cee83ec09e352ee3f4dcb0fe51f3c862b48f7bb85d11b'
-// }
-
-// test account 
-// module.exports = {
-//     publicKey: 'ak_CAUK33zDsoj7zuuMSqB8rJsWBioYdZTbQJEUFd5iJmtZaJHix',
-//     secretKey: '12324322d0b97dd63fbebac3d4e86d9dccd2ef9c244fe1e45b0ba836d0a50b3a1958fd7c2ed65cc9e17b10ea811e4d44f4cd8e11fc64cdc0d3b9a2a3d1df7b55'
-// }
-
-// local node
-// test account 
-// module.exports = {
-//     publicKey: 'ak_2mwRmUeYmfuW93ti9HMSUJzCk1EYcQEfikVSzgo6k2VghsWhgU',
-//     secretKey: 'bb9f0b01c8c9553cfbaf7ef81a50f977b1326801ebf7294d1c2cbccdedf27476e9bbf604e611b5460a3b3999e9771b6f60417d73ce7c5519e12f7e127a1225ca'
-// }
-
-
+// main coffee shop account
 module.exports = {
-    publicKey: 'ak_2F8Vo1DCTjWAgC5x5SxfgzcWhZWkLvAXANQa4uTvY9R8qdeTXK',
-    secretKey: '75cbc9a20dd8fcf178d4a87d9587bb0f5e6762454b55ea2ec594a9a8ace50f0aa3c73d937e90df3a915a98151f69890a8f90598d26548b1d1d0ff9d01acbeae6'
+    publicKey: 'ak_27c4YpfUuW4s9T6RBNpASNuDRiXSWQ93uZ5WxaUXfx1Lqibv33',
+    secretKey: '357985aa340a6dc64ad7fff331d478c9d2cee98f3cd5fbe1dd6b3c239647271392b142f128defdf72b6cee83ec09e352ee3f4dcb0fe51f3c862b48f7bb85d11b'
 }
 
 /***/ }),
